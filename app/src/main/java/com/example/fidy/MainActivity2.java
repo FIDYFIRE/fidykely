dc6b67b8456bd704b57bf02c5f050d4b752add85
FIDYFIRE <fidyfire@gmail.com>
2023-06-16 10:38:53 +0300

    INSERT

--- app/src/main/java/com/example/fidy/MainActivity2.java
+++ app/src/main/java/com/example/fidy/MainActivity2.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    EditText e2;
-
+    EditText e2, e3, e4;
+    Button inserer;
     SQLiteOpenHelper helper;
     SQLiteDatabase database;
     @Override
@@ -20,6 +22,9 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         e2 = findViewById(R.id.e2);
+        e3 = findViewById(R.id.e3);
+        e4 = findViewById(R.id.e4);
+        inserer = findViewById(R.id.inserer);
 
         /* Intent x = getIntent();
         e2.setText( x.getStringExtra("nom"));
@@ -37,5 +42,12 @@ public class MainActivity2 extends AppCompatActivity {
             }
         };
         database = helper.getWritableDatabase();
+
+        inserer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database.execSQL(" INSERT INTO Voitures (Nom, Prix, Km) VALUES ( '" + e2.getText() + "', "+ e3.getText()+", "+ e4.getText()+" )");
+            }
+        });
     }
 }
